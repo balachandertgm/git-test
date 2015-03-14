c28c1c17464e188c8ec7c72de9bd55c8daa588b1
balachandertgm <balatgm@gmail.com>
2015-03-14 17:07:19 +0530

    check db connection

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
 echo "Testing Php with git";
+
+
+	$mysql_host = "localhost";
+	$mysql_database = "test";
+	$mysql_user = "root";
+	$mysql_password = "mavinapps@123";
+
+
+//MySql Connection
+try
+{
+$con = mysql_connect($mysql_host,$mysql_user,$mysql_password);
+}
+catch(Exception $e)
+{
+	echo $e;
+}
+if(!$con)
+{
+	die('Connection Error'. mysql_error());
+}
+
+//Database Selection
+echo mysql_select_db($mysql_database,$con);
+
+echo "mysql working";
